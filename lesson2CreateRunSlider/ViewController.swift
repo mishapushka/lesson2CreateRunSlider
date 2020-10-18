1cc7a202bc8049e8d237bd6095d14f1489a333d9
Mikhail <bskfrost3@yandex.ru>
2020-10-18 15:29:12 +0700

    almost xcod finish

--- lesson2CreateRunSlider/ViewController.swift
+++ lesson2CreateRunSlider/ViewController.swift
@@ -11,28 +11,57 @@ class ViewController: UIViewController {
 
     @IBOutlet var presentView: UIView!
     
-    @IBOutlet var redView: UILabel!
     @IBOutlet var redScroll: UILabel!
     @IBOutlet var redSlider: UISlider!
+    
+    @IBOutlet var greenScroll: UILabel!
+    @IBOutlet var greenSlider: UISlider!
+    
+    @IBOutlet var blueScroll: UILabel!
+    @IBOutlet var blueSlider: UISlider!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redSlider.value = 0
-        redSlider.minimumValue = 0
-        redSlider.maximumValue = 1
+        //red
+        redSlider.value = 1
+        redSlider.minimumValue = 1
+        redSlider.maximumValue = 100
 
-        
         redScroll.text = String(redSlider.value)
+        
+        //green
+        greenSlider.value = 1
+        greenSlider.minimumValue = 1
+        greenSlider.maximumValue = 100
+        
+        greenScroll.text = String(greenSlider.value)
+        
+        //blue
+        blueSlider.value = 1
+        blueSlider.minimumValue = 1
+        blueSlider.maximumValue = 100
+        
+        blueScroll.text = String(blueSlider.value)
     }
 
     @IBAction func redSliderAction() {
-        let currentValue = CGFloat(redSlider.value)
         redScroll.text = String(redSlider.value)
-        presentView.backgroundColor = presentView.backgroundColor?.withAlphaComponent(currentValue)
-        
+        presentView.backgroundColor = UIColor(displayP3Red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
     }
     
+    @IBAction func greenSliderAction() {
+        greenScroll.text = String(greenSlider.value)
+        presentView.backgroundColor = UIColor(displayP3Red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0)
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueScroll.text = String(blueSlider.value)
+        presentView.backgroundColor = UIColor(displayP3Red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0)
+        
+    }
+   
 }
 
 
