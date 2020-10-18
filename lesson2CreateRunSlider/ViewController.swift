54889cb94292d533c87fb36078d227074fdf6739
Mikhail <bskfrost3@yandex.ru>
2020-10-18 17:47:57 +0700

    not ideas

--- lesson2CreateRunSlider/ViewController.swift
+++ lesson2CreateRunSlider/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     
     @IBOutlet var greenScroll: UILabel!
     @IBOutlet var greenSlider: UISlider!
-    
+
     @IBOutlet var blueScroll: UILabel!
     @IBOutlet var blueSlider: UISlider!
     
@@ -26,39 +26,51 @@ class ViewController: UIViewController {
         
         //red
         redSlider.value = 1
-        redSlider.minimumValue = 1
-        redSlider.maximumValue = 100
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 255
 
         redScroll.text = String(redSlider.value)
         
         //green
         greenSlider.value = 1
-        greenSlider.minimumValue = 1
-        greenSlider.maximumValue = 100
-        
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 255
+
         greenScroll.text = String(greenSlider.value)
-        
+
         //blue
         blueSlider.value = 1
-        blueSlider.minimumValue = 1
-        blueSlider.maximumValue = 100
-        
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 255
+
         blueScroll.text = String(blueSlider.value)
     }
 
     @IBAction func redSliderAction() {
+        let currentRedValue = CGFloat(redSlider.value)
+        let currentGreenValue = CGFloat(greenSlider.value)
+        let currentBlueValue = CGFloat(blueSlider.value)
         redScroll.text = String(redSlider.value)
-        presentView.backgroundColor = UIColor(displayP3Red: 1.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        presentView.backgroundColor = UIColor(displayP3Red: currentRedValue, green: currentGreenValue, blue: currentBlueValue, alpha: 1)
+        
     }
+        
     
     @IBAction func greenSliderAction() {
+        let currentRedValue = CGFloat(redSlider.value)
+        let currentGreenValue = CGFloat(greenSlider.value)
+        let currentBlueValue = CGFloat(blueSlider.value)
         greenScroll.text = String(greenSlider.value)
-        presentView.backgroundColor = UIColor(displayP3Red: 0.0, green: 1.0, blue: 0.0, alpha: 1.0)
+        presentView.backgroundColor = UIColor(displayP3Red: currentRedValue, green: currentGreenValue, blue: currentBlueValue, alpha: 1)
+        
     }
-    
+
     @IBAction func blueSliderAction() {
+        let currentRedValue = CGFloat(redSlider.value)
+        let currentGreenValue = CGFloat(greenSlider.value)
+        let currentBlueValue = CGFloat(blueSlider.value)
         blueScroll.text = String(blueSlider.value)
-        presentView.backgroundColor = UIColor(displayP3Red: 0.0, green: 0.0, blue: 1.0, alpha: 1.0)
+        presentView.backgroundColor = UIColor(displayP3Red: currentRedValue, green: currentGreenValue, blue: currentBlueValue, alpha: 1)
         
     }
    
